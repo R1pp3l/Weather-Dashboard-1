dbdbe1cdc461d97bc1fdaa0d4a0ac1a91bf2ca24
Victor Cabral <victorcabralweb@outlook.com>
2023-03-06 21:11:39 +0800

    Add temperature conversation scripts

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const apiKey = "7890fd97fa9a530f3159fe3ec8da1775";
 const searchInput = document.getElementById("searchInput");
+let temperatureUnit = "metric";
 
 const getDayName = (timestamp) => {
   const date = new Date(timestamp * 1000);
@@ -54,6 +55,49 @@ searchInput.addEventListener("keypress", (event) => {
           document.getElementById("currentLocation");
         const currentWeather = data.current;
 
+        const celsiusButton = document.getElementById("buttonCelsius");
+        const fahrenheitButton = document.getElementById("buttonFahrenheit");
+
+        function getTemperatureString(temp) {
+          if (temperatureUnit === "metric") {
+            return `${Math.round(temp)}°C`;
+          }
+          return `${Math.round(temp * 1.8 + 32)}°F`;
+        }
+        function updateCurrentWeather() {
+          currentTempElement.innerHTML = `${getTemperatureString(
+            currentWeather.temp
+          )}`;
+        }
+
+        function updateDailyWeather(daily) {
+          for (let i = 0; i < dailyWeatherElements.length; i += 1) {
+            const dailyWeather = daily[i];
+            const dailyWeatherElement = dailyWeatherElements[i];
+            const dayName = getDayName(dailyWeather.dt);
+            const iconUrl = `http://openweathermap.org/img/wn/${dailyWeather.weather[0].icon}.png`;
+            const minTemp = dailyWeather.temp.min;
+            const maxTemp = dailyWeather.temp.max;
+            const tempString = `${getTemperatureString(
+              minTemp
+            )} / ${getTemperatureString(maxTemp)}`;
+            dailyWeatherElement.innerHTML = `<p>${dayName}</p>
+            <img src="${iconUrl}" alt="${dailyWeather.weather[0].description}">
+            <p>${tempString}</p>`;
+          }
+        }
+        celsiusButton.addEventListener("click", () => {
+          temperatureUnit = "metric";
+          updateDailyWeather(data.daily);
+          updateCurrentWeather();
+        });
+
+        fahrenheitButton.addEventListener("click", () => {
+          temperatureUnit = "imperial";
+          updateDailyWeather(data.daily);
+          updateCurrentWeather();
+        });
+
         const humidityElement = document.getElementById("humidity");
         humidityElement.textContent = `${currentWeather.humidity}%`;
         document.getElementById("currentHumidity").classList.remove("hidden");
@@ -78,7 +122,9 @@ searchInput.addEventListener("keypress", (event) => {
         );
         currentDescriptionElement.innerHTML =
           data.current.weather[0].description;
-        currentTempElement.innerHTML = `${Math.round(currentWeather.temp)} °C`;
+        currentTempElement.innerHTML = `${Math.round(
+          currentWeather.temp
+        )}<span data-unit="metric">°C</span>`;
         const date = new Date(currentWeather.dt * 1000);
         const options = { weekday: "long", hour: "numeric", minute: "numeric" };
         currentDateTimeElement.innerHTML = date.toLocaleDateString(
